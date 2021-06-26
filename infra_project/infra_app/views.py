0d70fc294240ad81d3dd9af66f073f547728eeec
Anton Emelyanov <emelyaart@gmail.com>
2021-06-26 12:48:22 +0500

    bug fix

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Мой первый Action!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница!')
